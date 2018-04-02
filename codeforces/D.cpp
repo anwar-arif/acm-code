e744e95559177cc00f96223e9a54d467058b71e3
anwar-arif <anwarhossain221095@gmail.com>
2018-04-02 18:56:57 +0600

    footnotes.cpp_added

--- codeforces/D.cpp
+++ codeforces/D.cpp
@@ -6,8 +6,6 @@ const int inf = (int) 2e9 ;
 
 int main() {
 //    freopen("in.txt" , "r" , stdin ) ;
-    int n ; scanf("%d" , &n ) ;
-    printf("1\n") ;
-    printf("1 1\n") ;
+
     return 0 ;
 }
