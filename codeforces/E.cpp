236ac5b3bec88b4e0682c69f0791dc0bffdd72d0
anwar-arif <anwarhossain221095@gmail.com>
2019-10-19 12:24:50 +0600

    Add topcoder

--- codeforces/E.cpp
+++ codeforces/E.cpp
@@ -1,47 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = (int) 2e6 + 10;
+const int N = (int) 55;
 const int inf = (int) 2e8;
 
-char s[N];
-int n, m;
-long long dp[(1 << 20)], cnt[30][30];
+int dp[N][N][N][N], n;
+int csum[N][N], arr[N][N];
+char s[N][N];
 
-long long F(int mask, int p) {
-    if (p == m) return 0L;
-    long long &ret = dp[mask];
-    if (ret != -1) return ret;
-    ret = (long long) inf;
-    for (int i = 0; i < m; i++) {
-        if (!(mask & (1 << i))) {
-            long long add = 0L;
-            for (int j = 0; j < m; j++) {
-                if (i != j) {
-                    if (mask & (1 << j)) {
-                        add += p * cnt[i][j];
-                    } else {
-                        add -= p * cnt[i][j];
-                    }
-                }
-            }
-            ret = min(ret, add + F(mask | (1 << i), p + 1));
+void calc() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i + 1][j + 1] = s[i][j] == '#';
         }
+    }
+    memset(csum, 0, sizeof(csum));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            csum[i][j] = csum[i - 1][j] + csum[i][j - 1] - csum[i - 1][j - 1] + arr[i][j];
+        }
+    }
+}
+
+int black(int x1, int y1, int x2, int y2) {
+    int res = csum[x2][y2] - csum[x2][y1 - 1] - csum[x1 - 1][y2] + csum[x1 - 1][y1 - 1];
+    return res;
+}
+
+int F(int x1, int y1, int x2, int y2) {
+    if (black(x1, y1, x2, y2) == 0) return 0;
+    int &ret = dp[x1][y1][x2][y2];
+    if (ret != -1) return ret;
+    ret = max(x2 - x1 + 1, y2 - y1 + 1);
+    for (int x = x1; x < x2; x++) {
+        ret = min(ret, F(x1, y1, x, y2) + F(x + 1, y1, x2, y2));
+    }
+    for (int y = y1; y < y2; y++) {
+        ret = min(ret, F(x1, y1, x2, y) + F(x1, y + 1, x2, y2));
     }
     return ret;
 }
 
 int main() {
 //    freopen("in.txt", "r", stdin);
-    scanf("%d %d", &n, &m);
-    scanf("%s", &s);
-    memset(cnt, 0L, sizeof(cnt));
-    memset(dp, -1, sizeof(dp));
-    for (int i = 1; i < n; i++) {
-        int u = s[i - 1] - 'a';
-        int v = s[i] - 'a';
-        cnt[u][v]++, cnt[v][u]++;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", &s[i]);
     }
-    printf("%lld\n", F(0, 0));
+    calc();
+    memset(dp, -1, sizeof(dp));
+    printf("%d\n", F(1, 1, n, n));
     return 0 ;
 }
