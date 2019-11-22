54932ba5f9484db50a7c62ba41909fac91942837
anwar-arif <anwarhossain221095@gmail.com>
2019-11-22 21:05:01 +0600

    asdf

--- codeforces/B.cpp
+++ codeforces/B.cpp
@@ -4,30 +4,52 @@ using namespace std;
 const int N = (int) 2e6 + 10;
 const int inf = (int) 2e9;
 
-int cnt[N], n, mx = (int) 1e5;
-char s[N];
+int n;
+long long bits[70][3], m, csum[70];
 
 int main() {
 //    freopen("in.txt", "r", stdin);
-    memset(cnt, 0, sizeof(cnt));
-    scanf("%d", &n);
-    scanf("%s", &s);
-    for (int i = 0; i < n; i++) {
-        int a, b; scanf("%d %d", &a, &b);
-        int on = (s[i] == '1');
-        int nxt = b;
-        for (int j = 0; j <= mx; j += 1) {
-            if (j == nxt) {
-                on ^= 1;
-                nxt += a;
+    int T, cas = 0; scanf("%d", &T);
+    while (T--) {
+        scanf("%d %lld", &n, &m);
+        memset(bits, 0, sizeof(bits));
+        for (int i = 0; i < n; i++) {
+            long long x; scanf("%lld", &x);
+            for (int j = 0; j < 55; j++) {
+                if ((x & (1LL << j))) {
+                    bits[j][1]++;
+                } else {
+                    bits[j][0]++;
+                }
             }
-            cnt[j] += on;
         }
+        csum[0] = min(bits[0][0], bits[0][1]);
+        for (int j = 1; j < 55; j++) {
+            csum[j] = csum[j - 1] + (min(bits[j][0], bits[j][1]) * (1LL << j));
+        }
+        long long answer = 0L, cur = 0L;
+        int found = 0;
+        for (int j = 51; j >= 0; j--) {
+            if (j > 0 && csum[j - 1] > m) continue;
+            if (j > 0 && csum[j - 1] + cur > m) continue;
+            found = 1;
+            long long one_cost = bits[j][0] * (1LL << j);
+            if (j > 0) one_cost += csum[j - 1];
+            if (cur + one_cost <= m) {
+                answer |= (1LL << j);
+                cur += bits[j][0] * (1LL << j);
+            } else {
+                cur += bits[j][1] * (1LL << j);
+            }
+        }
+        if (answer == 0L) {
+            long long cur = 0L;
+            for (int i = 0; i < 52; i++) {
+                cur += (bits[i][1] * (1LL << i));
+            }
+            if (cur > m) answer = -1L;
+        }
+        printf("Case #%d: %lld\n", ++cas, answer);
     }
-    int ans = 0;
-    for (int i = 0; i <= mx; i++) {
-        ans = max(ans, cnt[i]);
-    }
-    printf("%d\n", ans);
     return 0 ;
 }
--- codeforces/C.cpp
+++ codeforces/C.cpp
@@ -1,59 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = (int) 2e5 + 10;
+const int N = (int) 1e6 + 10;
 const int inf = (int) 2e9;
 
-char seq[N];
-int dp[N][10][10], vis[N][10][10], n, cas = 0;
+vector <long long> primes;
+vector <bool> isPrime(N, 1);
 
-int F(int p, int f, int s, int m) {
-    if (p == n) return 1;
-    int &ret = dp[p][f][s];
-    if (vis[p][f][s] == cas) return ret;
-    vis[p][f][s] = cas, ret = 0;
-    int c = (seq[p] - '0');
-    if (c >= f && c <= m) ret |= F(p + 1, c, s, m);
-    if (c >= s) ret |= F(p + 1, f, c, m);
-    return ret;
-}
-
-void path(int p, int f, int s, int m) {
-    if (p == n) {
-        printf("\n");
-        return;
+void sieve() {
+    for (int i = 2; i < N; i++) {
+        if (isPrime[i]) {
+            for (int j = i + i; j < N; j += i) {
+                isPrime[j] = 0;
+            }
+        }
     }
-    int c = seq[p] - '0';
-    if (c >= f && c <= m && F(p + 1, c, s, m)) {
-        printf("1");
-        path(p + 1, c, s, m);
-        return;
-    }
-    if (c >= s && F(p + 1, f, c, m)) {
-        printf("2");
-        path(p + 1, f, c, m);
-        return;
+    for (int i = 2; i < N; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i + 0LL);
+        }
     }
 }
 
 int main() {
 //    freopen("in.txt", "r", stdin);
-    memset(dp, 0, sizeof(dp));
-    memset(vis, 0, sizeof(vis));
-    int T; scanf("%d", &T);
-    while (T--) {
-        scanf("%d", &n);
-        scanf("%s", &seq);
-        int can = 0;
-        for (int i = 0; i < 10; i++) {
-            cas += 1;
-            can |= F(0, 0, i, i);
-            if (can) {
-                path(0, 0, i, i);
-                break;
-            }
+    sieve();
+    long long n; scanf("%lld", &n);
+    long long answer = n;
+    for (long long p : primes) {
+        if (p > (n + 1) / 2) {
+            break;
         }
-        if (!can) printf("-\n");
+        if (n % p == 0) {
+            answer = p;
+            break;
+        }
     }
+    if (n > answer + answer) {
+        answer = 1;
+    }
+    printf("%lld\n", answer);
     return 0 ;
 }
