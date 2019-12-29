220e47f97295c08cf2f9fae248869b397595aaa1
anwar-arif <anwarhossain221095@gmail.com>
2019-12-30 00:53:03 +0600

    Revised bfs, dijkstra

--- ready precode/done/graph/BFS.cpp
+++ ready precode/done/graph/BFS.cpp
@@ -1,45 +1,42 @@
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-const int N = (int) 2e5 + 10 ;
-const int inf = (int) 2e9 ;
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
-vector< int > adj[N] ;
-int qu[N] , level[N] , vis[N] ;
+vector <int> adj[N];
+int level[N], vis[N];
+int nodes, edges;
 
-void bfs( int s , int t ) {
-    int l = 0 , r = 0 ;
-    qu[r++] = s ;
-    level[s] = 0 ; vis[s] = 1 ;
-    while( l < r ) {
-        int u = qu[l++] ;
-        int len = (int) adj[u].size() ;
-        for( int i = 0 ; i < len ; i++ ) {
-            int v = adj[u][i] ;
-            if( vis[v] == 0 ) {
-                level[v] = level[u] + 1 ;
-                par[v] = u ;
-                vis[v] = 1 ;
-                qu[r++] = v ;
+void add_edge(int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void bfs(int s, int t) {
+    queue <int> qu;
+    qu.push(s);
+    level[s] = 0, vis[s] = 1;
+    while (!qu.empty()) {
+        int u = qu.front(); qu.pop();
+        for (int v: adj[u]) {
+            if (vis[v] == 0) {
+                qu.push(v);
+                level[v] = level[u] + 1;
+                vis[v] = 1;
             }
         }
     }
 }
 
-void init( int nodes ) {
-    for( int i = 0 ; i <= nodes ; i++ ) {
-        par[i] = i ; vis[i] = 0 ;
-    }
-}
-
 int main() {
-    int n , m ; scanf("%d %d" , &n , &m ) ;
-    init( n ) ;
-    for( int i = 1 ; i <= n ; i++ ) {
-        int u , v ; scanf("%d %d" , &u , &v ) ;
-        adj[u].push_back(u) ;
-        adj[v].push_back(v) ;
+//    freopen("in.txt", "r", stdin);
+    scanf("%d %d", &nodes, &edges);
+    memset(vis, 0, sizeof(vis));
+    for (int i = 0; i < nodes; i++) {
+        int u, v; scanf("%d %d", &u, &v);
+        add_edge(u, v);
     }
-    bfs(1 , n) ;
-    return 0 ;
+    bfs(1, nodes);
+    return 0;
 }
--- ready precode/done/graph/Dijkstra.cpp
+++ ready precode/done/graph/Dijkstra.cpp
@@ -1,64 +1,67 @@
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-const int N = (int) 2e5 + 10 ;
-const int inf = (int) 2e9 ;
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
-struct edge{
-    int u , w ;
+struct edge {
+    int u, w;
     edge(){}
-    edge( int _u , int _w ) {
-        u = _u ; w = _w ;
+    edge(int _u, int _w) {
+        u = _u, w = _w;
     }
-    bool operator < (const edge &p ) const {
-        return w > p.w ;
+    bool operator < (const edge &p) const {
+        return w > p.w;
     }
 };
 
-vector< int > adj[N] , cost[N] ;
-int level[N] ;
+vector <pair<int, int>> adj[N];
+int nodes, edges, level[N];
 
-void init( int nodes ) {
-    for( int i = 0 ; i <= nodes ; i++ ) {
-        adj[i].clear() ;
-        cost[i].clear() ;
-        level[i] = inf ;
+void init(int n) {
+    for (int i = 0; i <= n; i++) {
+        adj[i].clear();
+        level[i] = inf;
     }
 }
 
-int Dij( int s , int t ) {
-    priority_queue< edge > pq ;
-    pq.push( edge(s , 0) ) ;
-    level[s] = 0 ;
-    while( !pq.empty() ) {
-        edge top = pq.top() ; pq.pop() ;
-        int u = top.u ;
-        if( u == t ) return level[u] ;
-        int len = (int) adj[u].size() ;
-        for( int i = 0 ; i < len ; i++ ) {
-            int v = adj[u][i] ;
-            if( level[v] > level[u] + cost[u][i] ) {
-                level[v] = level[u] + cost[u][i] ;
-                pq.push( edge(v , level[v]) ) ;
+int Dij(int s, int t) {
+    priority_queue <edge> pq;
+    pq.push(edge(s, 0));
+    level[s] = 0;
+
+    while (!pq.empty()) {
+        edge top = pq.top(); pq.pop();
+        int u = top.u;
+        if (u == t) return level[u];
+
+        for (auto p: adj[u]) {
+            int v = p.first, c = p.second;
+            if (level[u] + c < level[v]) {
+                level[v] = level[u] + c;
+                pq.push(edge(v, level[v]));
             }
         }
     }
-    return -1 ;
+
+    return -1;
 }
 
 int main() {
-    int n , m , s , t ;
-    init( n ) ;
-    scanf("%d %d %d %d" , &n , &m , &s , &t ) ;
-    for( int i = 1 ; i <= n ; i++ ) {
-        int u , v , c ; scanf("%d %d %d" , &u , &v , &c ) ;
-        adj[u].push_back(v) ;
-        adj[v].push_back(u) ;
-        cost[u].push_back(c) ;
-        cost[v].push_back(c) ;
+//    freopen("in.txt", "r", stdin);
+    scanf("%d %d", &nodes, &edges);
+    int s, t; scanf("%d %d", &s, &t);
+    init(nodes);
+
+    for (int i = 1; i <= nodes; i++) {
+        int u, v, c; scanf("%d %d %d", &u, &v, &c);
+        adj[u].push_back({v, c});
+        adj[v].push_back({u, c});
     }
-    int ans = Dij( s , t ) ;
-    if( ans == -1 ) printf("Not Reachable\n") ;
-    else printf("Minimum Distance %d\n" , ans ) ;
-    return 0 ;
+
+    int answer = Dij(s, t);
+    if (answer == -1) printf("Not Reachable\n");
+    else printf("Minimum Distance %d\n", answer);
+
+    return 0;
 }
