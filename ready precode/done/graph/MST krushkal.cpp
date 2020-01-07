e65f5e4209b1cf0b02ea3662bd294829344b06c1
anwar-arif <anwarhossain221095@gmail.com>
2020-01-08 00:39:27 +0600

    Revised mst and articulation point

--- ready precode/done/graph/MST krushkal.cpp
+++ ready precode/done/graph/MST krushkal.cpp
@@ -1,60 +1,59 @@
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-const int N = (int) 2e5 + 10 ;
-const int inf = (int) 2e9 ;
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
 struct edge {
-    int u , v , w ;
+    int u, v, w;
     edge(){}
-    edge( int _u , int _v , int _w ) {
-        u = _u ; v = _v ; w = _w ;
+    edge(int _u, int _v, int _w) {
+        u = _u, v = _v, w = _w;
     }
-    bool operator < (const edge &p ) const {
-        return w < p.w ;
+    bool operator < (const edge &p) const {
+        return w < p.w;
     }
 };
 
-vector< edge > adj ;
-int par[N] ;
+vector <edge> adj;
+int par[N], nodes, edges;
 
-int findpar( int r ) {
-    return ( par[r] == r ) ? r : par[r] = findpar( par[r] ) ;
+void init(int n) {
+    for (int i = 0; i <= n; i++) {
+        par[i] = i;
+    }
+    adj.clear();
 }
 
-int mst( int nodes ) {
-    sort( adj.begin() , adj.end() ) ;
-    for( int i = 0 ; i <= nodes ; i++ ) par[i] = i ;
-    int cnt = 0 , sum = 0 ;
-    int len = (int) adj.size() ;
-    for( int i = 0 ; i < len ; i++ ) {
-        int u = findpar( adj[i].u ) ;
-        int v = findpar( adj[i].v ) ;
-        if( u != v ) {
-            par[u] = v ;
-            cnt += 1 ;
-            sum += adj[i].w ;
-            if( cnt == nodes - 1 ) break ;
+int findpar(int r) {
+    return (par[r] == r) ? r : par[r] = findpar(par[r]);
+}
+
+void mst() {
+    sort(adj.begin(), adj.end());
+    int cnt = 0, sum = 0;
+    int len = (int) adj.size();
+    for (int i = 0; i < len; i++) {
+        int u = findpar(adj[i].u);
+        int v = findpar(adj[i].v);
+        if (u != v) {
+            par[u] = v;
+            cnt += 1;
+            sum += adj[i].w;
+            if (cnt == nodes - 1) break;
         }
     }
-    return sum ;
-}
-
-void init( int nodes ) {
-    adj.clear() ;
+    printf("mst: %d\n", sum);
 }
 
 int main() {
-    int n , m ; scanf("%d %d" , &n , &m ) ;
-    init( n ) ;
-    for( int i = 1 ; i <= m ; i++ ) {
-        int u , v , w ; scanf("%d %d %d" , &u , &v , &w ) ;
-        adj.push_back( edge(u , v , w) ) ;
+//    freopen("in.txt", "r", stdin);
+    scanf("%d %d", &nodes, &edges);
+    init(nodes);
+    for (int i = 0; i < edges; i++) {
+        int u, v, w; scanf("%d %d %d", &u, &v, &w);
+        adj.push_back(edge(u, v, w));
     }
-    cout << mst(n) << endl ;
-    return 0 ;
+    mst();
+    return 0;
 }
-/*
-Complexity : First sort the edges and then lenear loop of them.
-             so complexity is O(ElogE) where E = number of edge;
-*/
--- ready precode/done/graph/articulation point.cpp
+++ ready precode/done/graph/articulation point.cpp
@@ -1,97 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-const int N = 1e6 + 5;
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
-vector<int>g[N] ;
+vector <int> adj[N];
+int nodes, edges, depth;
+int par[N], vis[N], dis[N], low[N], art[N];
 
-bool vis[N] , art[N];
-
-int par[N] , dis[N] , fin[N] , depth;
-
-void reset(){
-
-    depth = 0;
-
-    rep(i,0,N-2){
-        g[i].clear();
-        vis[i] = false;
-        par[i] = -1;
-        art[i] = false;
+void init(int n) {
+    for (int i = 0; i <= n; i++) {
+        vis[i] = 0, par[i] = -1, art[i] = 0;
+        adj[i].clear();
     }
 }
 
-void dfs(int u){
-
-    vis[u] = true;
-    dis[u] = fin[u] = depth++;
-
+void dfs(int u) {
+    vis[u] = 1;
     int child = 0;
+    dis[u] = low[u] = depth++;
 
-    int len = SZ(g[u]);
+    for (int v: adj[u]) {
+        if (!vis[v]) {
+            par[v] = u, child += 1;
+            dfs(v);
+            low[u] = min(low[u], low[v]);
 
-    for(int i = 0 ;i < len ;i++){
-
-        int v = g[u][i] ;
-
-        if(!vis[v]){
-
-            ++child;
-            par[v] = u;
-
-            dfs(v) ;
-
-            dis[u] = min(dis[u] , dis[v]);
-
-            if(par[u] != -1){
-                if(dis[v] >= fin[u]){
-                    art[u] = true;
+            if (par[u] != -1) {
+                if (dis[u] <= low[v]) {
+                    art[u] = 1;
                 }
             }
-        }
-        else if(v != par[u]){
-
-            dis[u] = min(dis[u] , fin[v]);
+        } else if (v != par[u]) {
+            low[u] = min(low[u], dis[v]);
         }
     }
-    if(par[u] == -1){
 
-        if(child > 1){
-            art[u] = true;
-        }
+    if (par[u] == -1 && child > 1) {
+        art[u] = 1;
     }
 }
 
-int main(){
-    int tst,cas = 0;
-    sc1i(tst) ;
+int main() {
+//    freopen("in.txt", "r", stdin);
+    scanf("%d %d", &nodes, &edges);
+    init(nodes);
 
-    int n , m;
-
-    while(tst--){
-        sc2i(n,m);
-
-        reset();
-
-        int u , v;
-        rep(i,1,m){
-
-            sc2i(u,v) ;
-
-            g[u].pb(v) ;
-            g[v].pb(u) ;
-        }
-
-        rep(i,1,n){
-
-            if(!vis[i])dfs(i);
-        }
-
-        int tot = 0;
-
-        rep(i,1,n){
-
-            tot += (art[i] == true);
-        }
-        pf("Case %d: %d\n",++cas,tot);
+    for (int i = 0; i < edges; i++) {
+        int u, v; scanf("%d %d", &u, &v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-}
 
+    for (int i = 1; i <= nodes; i++) {
+        if (vis[i] == 0) {
+            depth = 0;
+            dfs(i);
+        }
+    }
+
+    printf("Articulation points: ");
+
+    for (int i = 1; i <= nodes; i++) {
+        if (art[i] == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
