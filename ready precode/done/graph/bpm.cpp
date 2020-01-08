feb91843cf8892c9d4e81c0006b22531f09e52e0
anwar-arif <anwarhossain221095@gmail.com>
2020-01-09 00:02:28 +0600

    Revised bpm

--- ready precode/done/graph/bpm.cpp
+++ ready precode/done/graph/bpm.cpp
@@ -1,59 +1,55 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int N = 500+10;
 
-vector<int>graph[N];
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
-int match[N] ;
-bool vis[N] ;
+vector <int> adj[N];
+int n, m;
+int match[N], vis[N];
 
-bool dfs( int u ) {
-    int len = (int) graph[u].size() ;
-    for( int i = 0 ; i < len ; i++ ) {
-        int v = graph[u][i] ;
-        if( !vis[v] ) {
-            vis[v] = true ;
-            if( match[v] == -1 || dfs( match[v] ) ) {
-                match[v] = u ;
-                return true ;
+bool dfs(int u) {
+    for (int v: adj[u]) {
+        if (!vis[v]) {
+            vis[v] = 1;
+            if (match[v] == -1 || dfs(match[v])) {
+                match[v] = u;
+                return true;
             }
         }
     }
-    return false ;
+    return false;
 }
 
 int bpm() {
-    int ret = 0 ;
-    memset( match , -1 , sizeof(match) ) ;
-    for( int i = 0 ; i < c ; i++ ) {
-        memset( vis , false , sizeof(vis) ) ;
-        if( dfs(i) ) {
-            ++ret ;
+    int ret = 0;
+    memset(match, -1, sizeof(match));
+
+    for (int i = 1; i <= n; i++) {
+        memset(vis, 0, sizeof(vis));
+        if (dfs(i)) {
+            ret += 1;
         }
     }
-    return ret ;
+
+    return ret;
 }
 
-int main()
-{
-      int tst,cas = 0;
-      sc1i(tst);
-
-      while(tst--){
-          for(int i = 0; i < n;i++){
-
-            sc1i(x);
-
-            for(int k = 1; k <= x;k++){
-
-                sc1i(u);
-                graph[i].pb(u);
-            }
+int main() {
+//    freopen("in.txt", "r", stdin);
+    scanf("%d %d", &n, &m);
+    for (int u = 1; u <= n; u++) {
+        int prefer; scanf("%d", &prefer);
+        for (int j = 1; j <= prefer; j++) {
+            int v; scanf("%d", &v);
+            adj[u].push_back(v);
         }
-        int ans = bpm();
-        pf("Case %d: %d\n",++cas,n - ans);
-      }
-      return 0;
+    }
+
+    int answer = bpm();
+    printf("%d\n", answer);
+
+    return 0;
 }
 /**
     complexity = N*(M+E)
