7708e1bd74cbf002459b7f606525e304f1fd9601
anwar-arif <anwarhossain221095@gmail.com>
2020-01-26 15:02:45 +0600

    Revised hashing

--- ready precode/done/data structure/Hashing.cpp
+++ ready precode/done/data structure/Hashing.cpp
@@ -1,106 +1,59 @@
 #include<bits/stdc++.h>
-const int N = 500 + 5;
+using namespace std;
 
-using ll = long long;
+const int N = (int) 2e6 + 10;
+const int inf = (int) 2e9;
 
-const ll M1 = 10000009;
-const ll B1 = 33;
-const ll M2 = 10000007;
-const ll B2 = 163;
+const long long base[] = {33LL, 163LL};
+const long long mod[] = {10000007LL, 10000009LL};
 
+// Hash and power values
+long long H[2][N], P[2][N];
 string s;
 
-ll h1[100010] , h2[100010];
-ll pow1[100010] , pow2[100010];
-
 void init() {
-    int n = s.size() , i;
+    P[0][0] = P[1][0] = 1LL;
+    H[0][0] = H[1][0] = 0LL;
 
-    pow1[0] = pow2[0] = 1;
-    h1[0] = h2[0] = 0 ;
+    int n = (int) s.size();
 
-    ///  calculate power
-    for(i = 1; i <= n; i++) {
-        pow1[i] = (B1 * pow1[i-1]) % M1;
-        pow2[i] = (B2 * pow2[i-1]) % M2;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 1; j <= n; j++) {
+            P[i][j] = (base[i] * P[i][j - 1]) % mod[i];
+        }
     }
 
-    for(i = 1; i <= n; i++) /// calculate hash value
-    {
-        h1[i] = (B1 * h1[i-1] + s[i-1] - 'a' + 1) % M1;
-
-        h2[i] = (B2 * h2[i-1] + s[i-1] - 'a' + 1) % M2;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 1; j <= n; j++) {
+            H[i][j] = (H[i][j - 1] * base[i] + (s[j] - 'a' + 1)) % mod[i];
+        }
     }
 }
 
-ll getHash1(int l , int r)/// get hash value of range l to r
-{
+long long getHash(int id, int l, int r) {
     int len = r - l + 1;
-
-    ll ret = (h1[r] - (h1[l-1] * pow1[len]) % M1 + M1) % M1;
-
-    return ret;
+    return (H[id][r] - ((H[id][l - 1] * P[id][len]) % mod[id]) + mod[id]) % mod[id];
 }
 
-ll getHash2(int l , int r)/// double hash
-{
-    int len = r - l + 1;
-
-    ll ret = (h2[r] - (h2[l-1] * pow2[len]) % M2 + M2) % M2;
-
-    return ret;
+pair <long long, long long> getHashPair(int l, int r) {
+    return {getHash(0, l, r), getHash(1, l ,r)};
 }
 
-int main()
-{
-    int n , q , i;
-    while( cin>>s )
-    {
-        n = s.size();
-        init();/// set hash value
-        sc1i(q);
-        while(q--)
-        {
-            int x1,y1,x2,y2;
 
-            sc2i(x1 , y1);
-            sc2i(x2 , y2);
-
-            if(y1 - x1 == y2 - x2)/// length equal
-            {
-                pair < ll , ll > H1(getHash1(x1,y1),getHash2(x1,y1));
-                pair < ll , ll > H2(getHash1(x2,y2),getHash2(x2,y2));
-                if(H1 == H2)
-                    pf("Yes\n");
-                else
-                    pf("No\n");
+int main() {
+//    freopen("in.txt", "r", stdin);
+    while (cin >> s) {
+        init();
+        int queries; scanf("%d", &queries);
+        while (queries--) {
+            int a, b; scanf("%d %d", &a, &b);
+            int c, d; scanf("%d %d", &c, &d);
+            if (b - a == d - c) {
+                printf("%s\n", (getHashPair(a, b) == getHashPair(c, d)) ? "Yes" : "No");
+            } else {
+                printf("No");
             }
-            else
-                printf("No\n");
         }
     }
     return 0;
 }
-/*
-const int BASE = 31;
-const int MOD = (int)1e9+7;
-
-map<long long, int> hash;
-    for (int i = 0; i < n; ++i) {
-        long long hashVal = 0;
-
-        for (char ch : v[i]) {
-            int d = ch - 'a' + 1;
-            hashVal = hashVal * BASE + d;
-            if (hashVal >= MOD) {
-                hashVal %= MOD;
-            }
-
-            if (hash.find(hashVal) != hash.end()) {
-                hash.erase(hash.find(hashVal));
-            }
-        }
-        hash[hashVal] = i;
-
-    }
-*/
