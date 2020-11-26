bbbb4c52641440878cf3e5caf27dfb8614d72671
Anwar Hossain <anwarhossain221095@gmail.com>
2020-11-27 02:09:39 +0600

    Update segment tree from tree

--- ready precode/done/data structure/segment tree from a tree.cpp
+++ ready precode/done/data structure/segment tree from a tree.cpp
@@ -1,76 +1,76 @@
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-const int N = 200000 + 10 ;
+const int N = 200000 + 10;
 
-int tree[4*N] , lazy[4*N] , arr[N] , tym = 0 ;
-int start[N] , fin[N] , col[N] ;
+int tree[4 * N], lazy[4 * N], arr[N], tym = 0;
+int start[N], fin[N], col[N];
 
-vector< int > g[N] ;
+vector<int> g[N];
 
-void dfs( int  u , int p ) {
-    start[u] = ++tym ;
-    arr[tym] = col[u] ;
-    int len = (int)g[u].size() ;
-    for( int i = 0 ; i < len ; i++ ) {
-        int v = g[u][i] ;
-        if( v == p ) continue ;
-        dfs( v , u ) ;
+void dfs(int u, int p) {
+    start[u] = ++tym;
+    arr[tym] = col[u];
+    int len = (int)g[u].size();
+    for (int i = 0; i < len; i++) {
+        int v = g[u][i];
+        if (v == p) continue;
+        dfs(v, u);
     }
-    fin[u] = tym ;
+    fin[u] = tym;
 }
 
-void mrg( int node , int lft , int rt ) {
-    tree[node] = tree[lft] + tree[rt] ;
+void mrg(int node, int lft, int rt) {
+    tree[node] = tree[lft] + tree[rt];
 }
 
-void build(int node , int b , int e ) {
-    if( b == e ) {
-        tree[node] = arr[b] ;
-        lazy[node] = 0 ;
-        return ;
+void build (int node, int b, int e) {
+    if (b == e) {
+        tree[node] = arr[b];
+        lazy[node] = 0;
+        return;
     }
-    int lft = 2*node , rt = lft + 1 , mid = ( b + e ) / 2 ;
-    build( lft , b , mid ) ;
-    build( rt , mid+1 , e ) ;
-    mrg( node , lft , rt ) ;
-    lazy[node] = 0 ;
+    int lft = 2 * node , rt = lft + 1, mid = (b + e) / 2;
+    build(lft, b, mid);
+    build(rt, mid + 1, e);
+    mrg(node, lft, rt );
+    lazy[node] = 0;
 }
 
-void push_down( int node , int lft , int rt , int b , int e ) {
-    if( (lazy[node]&1) ) {
-        tree[node] = ( e - b + 1 ) - tree[node] ;
-        if( b != e ) {
-            ++lazy[lft] ;
-            ++lazy[rt] ;
+void push_down(int node, int lft, int rt, int b, int e) {
+    if ((lazy[node] & 1)) {
+        tree[node] = (e - b + 1) - tree[node];
+        if (b != e) {
+            ++lazy[lft];
+            ++lazy[rt];
         }
     }
-    lazy[node] = 0 ;
+    lazy[node] = 0;
 }
 
-void update( int node , int b , int e , int i , int j ) {
-    int lft = 2*node , rt = lft + 1 , mid = ( b + e ) / 2 ;
-    push_down( node , lft , rt , b , e ) ;
-    if( e < i || b > j ) return ;
-    if( b >= i && e <= j ) {
-        ++lazy[node] ;
-        push_down(node , lft , rt , b , e ) ;
-        return ;
+void update(int node, int b, int e, int i, int j) {
+    int lft = 2 * node, rt = lft + 1, mid = (b + e) / 2;
+    push_down(node, lft, rt, b, e );
+    if (e < i || b > j) return ;
+    if (b >= i && e <= j) {
+        ++lazy[node];
+        push_down(node, lft, rt, b, e);
+        return;
     }
-    update( lft , b , mid , i , j ) ;
-    update( rt , mid+1 , e , i , j ) ;
-    mrg( node , lft , rt ) ;
+    update(lft, b, mid, i, j);
+    update(rt, mid + 1, e, i, j);
+    mrg(node, lft, rt);
 }
 
-int query( int node , int b , int e , int i , int j ) {
-    int lft = 2*node , rt = lft + 1 , mid = ( b + e ) / 2 ;
-    push_down( node , lft , rt , b , e ) ;
-    if( e < i || b > j ) return 0 ;
-    if( b >= i && e <= j ) {
-        push_down( node , lft , rt , b , e ) ;
-        return tree[node] ;
+int query(int node, int b, int e, int i, int j) {
+    int lft = 2 * node, rt = lft + 1, mid = (b + e) / 2;
+    push_down(node, lft, rt, b, e);
+    if (e < i || b > j) return 0;
+    if (b >= i && e <= j) {
+        push_down(node, lft, rt, b, e);
+        return tree[node];
     }
-    return ( query(lft , b , mid , i , j ) + query(rt , mid+1 , e , i , j ) ) ;
+    return (query(lft, b, mid, i, j) + query(rt, mid + 1, e, i, j));
 }
 /***
     pow v describes a task to switch lights in the subtree of vertex v.
@@ -78,29 +78,29 @@ int query( int node , int b , int e , int i , int j ) {
 */
 
 int main() {
-    freopen("input.txt" , "r" , stdin ) ;
-    int n , p , u ;
-    scanf("%d" , &n ) ;
-    for( int i = 2 ; i <= n ; i++ ) {
-        scanf("%d" , &p ) ;
-        g[p].push_back(i) ;
+    freopen("input.txt", "r", stdin);
+    int n, p, u;
+    scanf("%d", &n);
+    for (int i = 2; i <= n; i++) {
+        scanf("%d", &p);
+        g[p].push_back(i);
     }
-    for( int i = 1 ; i <= n ; i++ ) {
-        scanf("%d" , &col[i] ) ;
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &col[i]);
     }
-    dfs( 1 , -1 ) ;
-    build( 1 , 1 , n ) ;
-    int q ; scanf("%d" , &q ) ;
-    char typ[10] ;
-    while( q-- ) {
-        scanf("%s" , typ ) ;
-        scanf("%d" , &u ) ;
-        if( strcmp("get" , typ ) == 0 ) {
-            int ans = query( 1 , 1 , n , start[u] , fin[u] ) ;
-            printf("%d\n" , ans ) ;
+    dfs(1, -1);
+    build(1, 1, n);
+    int q ; scanf("%d", &q);
+    char typ[10];
+    while (q--) {
+        scanf("%s", typ);
+        scanf("%d", &u);
+        if (strcmp("get", typ) == 0) {
+            int ans = query(1, 1, n, start[u], fin[u]);
+            printf("%d\n", ans);
         }
         else{
-            update( 1 , 1 , n , start[u] , fin[u] ) ;
+            update(1, 1, n ,start[u], fin[u]);
         }
     }
     return 0 ;
