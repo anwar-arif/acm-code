2aba6197b135d3b0613d95afaa3deedcc49b84f0
Anwar Hossain <anwarhossain221095@gmail.com>
2020-12-05 22:03:23 +0600

    Update KMP

--- ready precode/done/string & others/KMP.cpp
+++ ready precode/done/string & others/KMP.cpp
@@ -1,65 +1,80 @@
 #include<bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-const int N = (int) 2e6 + 10 ;
-const int inf = (int) 2e9 ;
+class KMP {
+public:
+    string text = "";
+    string pattern = "";
+    vector<int> lps;
 
-int *lps ;
-string text , pat ;
+    KMP(string _text, string _pattern) {
+        text = _text;
+        pattern = _pattern;
+        calculateLPS();
+    }
 
-void cal_lps() {
-    int len = (int) pat.size() , indx = 0 ;
-    lps = new int [len + 2] ;
-    lps[0] = 0 ;
-    for( int i = 1 ; i < len ; ) {
-        if( pat[i] == pat[indx] ) {
-            lps[i] = indx + 1 ;
-            i++ , indx++ ;
-        }
-        else {
-            if( indx != 0 ) {
-                indx = lps[indx - 1] ;
-            }
-            else {
-                lps[i] = 0 ;
-                i++ ;
+    /* calculate longest prefix which is also a suffix*/
+    void calculateLPS() {
+        int len = (int) pattern.size();
+        lps = vector<int> (len, 0);
+
+        for (int i = 1, index = 0; i < len;) {
+            if (pattern[i] == pattern[index]) {
+                lps[i] = index + 1;
+                i++, index++;
+            } else {
+                if (index != 0) {
+                    index = lps[index - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
     }
-}
 
-void kmp() {
-    cal_lps() ;
-    int i = 0 , j = 0 ;
-    int text_len = (int) text.size() ;
-    int pat_len = (int) pat.size() ;
+    vector<int> substrOccurrence() {
+        vector<int> answer;
 
-    ///i for text and j for pat
-    while( i < text_len ) {
-        if( text[i] == pat[j] ) {
-            i++ , j++ ;
-        }
-        if( j == pat_len ) {
-            printf("pat found at %d\n" , i - j ) ;
-            j = lps[j - 1] ;
-//            return (i - j) ;
-        }
-        else if( i < text_len && text[i] != pat[j] ) {
-            if( j != 0 ) {
-                j = lps[j - 1] ;
+        int textLen = (int) text.size();
+        int patternLen = (int) pattern.size();
+        int i = 0, j = 0;
+
+        while (i < textLen) {
+            if (text[i] == pattern[j]) {
+                i++, j++;
             }
-            else {
-                i++ ;
+            if (j == patternLen) {
+                answer.push_back(i - j);
+                j = lps[j - 1];
+            }
+            else if (i < textLen && text[i] != pattern[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
+
+        return answer;
     }
-}
+};
 
-int main(){
-//    freopen("in.txt" , "r" , stdin ) ;
-    cin >> pat >> text ;
-    kmp() ;
-    return 0 ;
-}
+int main() {
+    string text = "codeforces";
+    string pattern = "e";
 
-/* complexity : O(m + n )*/
+    KMP kmp(text, pattern);
+    vector<int> positions = kmp.substrOccurrence();
+
+    for (int pos: positions) {
+        cout << pos << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+/*
+Time complexity O(textLen + patternLen)
+*/
