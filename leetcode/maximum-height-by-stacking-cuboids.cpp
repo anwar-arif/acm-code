2121da01f1af5ecd749cde5c5d064f9ab48ae093
Anwar Hossain <anwarhossain221095@gmail.com>
2020-12-26 02:06:21 +0600

    leetcode

---
+++ leetcode/maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file maximum-height-by-stacking-cuboids
+ * @author Anwar Hossain <anwararif727@gmail.com>
+ *
+ * @date Saturday December 26 2020
+ *
+ * @brief 
+ */
+class Solution {
+public:
+    int inf = (int) 2e9;
+    vector<vector<int>> adj;
+    
+    int can(vector<int> &a, vector<int> &b) {
+        for (int k = 0; k < 3; k++) {
+            if (a[k] < b[k]) return false;
+        }
+        return true;
+    }
+    
+    int solve(int cur, vector<int> &dp, vector<vector<int>>& cuboids) {
+        int n = (int) cuboids.size();
+        
+        int &ret = dp[cur];
+        if (ret != -1) return ret;
+        
+        ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (cur == i) continue;
+            if (can(cuboids[cur], cuboids[i])) {
+                ret = max(ret, solve(i, dp, cuboids));
+            }
+        }
+        
+        ret += cuboids[cur][2];
+        return ret;
+    }
+    
+    int maxHeight(vector<vector<int>>& cuboids) {
+        int len = (int) cuboids.size();
+        
+        for (int i = 0; i < len; i++) {
+            sort(cuboids[i].begin(), cuboids[i].end());
+        }
+        
+        vector<int> dp(len + 1, -1);
+        
+        int answer = -inf;
+        for (int i = 0; i < len; i++) {
+            answer = max(answer, solve(i, dp, cuboids));
+        }
+        
+        return answer;
+    }
+};
