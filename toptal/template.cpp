f8b744074d908d4498b3e701fe603861e75bff40
Anwar Hossain <anwararif727@gmail.com>
2021-12-04 23:44:23 +0600

    fixed date format

--- toptal/template.cpp
+++ toptal/template.cpp
@@ -49,22 +49,37 @@ struct StringUtil {
 // https://en.cppreference.com/w/c/chrono/strftime
 // https://www.cplusplus.com/reference/ctime/strftime/
 struct DateUtil {
-    void testing() {
+    /*
+        struct tm
+        {
+        int tm_sec;			// Seconds.	[0-60] (1 leap second) 
+        int tm_min;			// Minutes.	[0-59] 
+        int tm_hour;			// Hours.	[0-23] 
+        int tm_mday;			// Day.		[1-31] 
+        int tm_mon;			// Month.	[0-11] 
+        int tm_year;			// Year	- 1900.
+        int tm_wday;			// Day of week.	[0-6] 
+        int tm_yday;			// Days in year.[0-365]	
+        int tm_isdst;			// DST.		[-1/0/1]
+    */
+    tm* getTime(int year, int month, int day, int hour = 0, int minute = 0, int second = 0) {
         time_t now = time(0);
-        char *dt = ctime(&now);
-        cout << dt << endl;
+        tm* ltm = localtime(&now);
+        ltm->tm_year = year - 1900;
+        ltm->tm_mon = month - 1;
+        ltm->tm_mday = day;
+        ltm->tm_hour = hour;
+        ltm->tm_min = minute;
+        ltm->tm_sec = second;
+        return ltm;
+    }
 
-        tm *ltm = localtime(&now);
-        cout << "Year:" << 1900 + ltm->tm_year<<endl;
-        cout << "Month: "<< 1 + ltm->tm_mon<< endl;
-        cout << "Day: "<< ltm->tm_mday << endl;
-        cout << "Time: "<< 5+ltm->tm_hour << ":";
-        cout << 30+ltm->tm_min << ":";
-        cout << ltm->tm_sec << endl;
-
+    string timeToString(tm* ltm, string format = "") {
+        if (format.empty()) format = "%d/%m/%Y %H:%M:%S";
+        const char* fmt = format.c_str();
         char buffer[256];
         strftime(buffer, sizeof(buffer), "%d/%m/%Y %H:%M:%S", ltm);
-        cout << buffer << endl;
+        return string(buffer);
     }
 
     tm parseDateTime() {
@@ -76,22 +91,23 @@ struct DateUtil {
         puts(buf);
         return tm;
     }
+
+    void testing() {
+        tm *ltm = getTime(2021, 12, 4);
+
+        cout << "Year:" << 1900 + ltm->tm_year<<endl;
+        cout << "Month: "<< 1 + ltm->tm_mon<< endl;
+        cout << "Day: "<< ltm->tm_mday << endl;
+        cout << "Time: "<< ltm->tm_hour << ":";
+        cout << ltm->tm_min << ":";
+        cout << ltm->tm_sec << endl;
+
+        cout << timeToString(ltm) << endl;
+
+    }
     
 };
 
 int main() {
-    // string str = " anwar,hossain arif ";
-    // cout << str << endl;
-    // cout << StringUtil{}.trim(str) << endl;
-    // auto splits = StringUtil{}.split(str, ',');
-    // cout << splits.size() << endl;
-    // for (auto s: splits) {
-    //     cout << s << endl;
-    // }
-    // str = "anwar anwar anwar";
-    // StringUtil{}.replace(str, "anwar", "anwar2");
-    // cout << str << endl;
-    // DateUtil{}.testing();
-    DateUtil{}.parseDateTime();
-    
+    DateUtil{}.testing();
 }
