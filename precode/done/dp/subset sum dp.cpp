cbfe2d48aa9fa211eaa1ef3b8b670d4647dcb35d
anwar-arif <anwararif727@gmail.com>
2025-10-19 15:51:14 +0600

    refactor

--- precode/done/dp/subset sum dp.cpp
+++ precode/done/dp/subset sum dp.cpp
@@ -1,35 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = (int) 10000 + 10;
-const int M = 55;
+const int N = 10000+5;
 
-int n = 4, dp[N][M], sum;
-int a[] = {2, 3, 7, 8};
+/*
+S[i][j] is the solution for the problem with the first i numbers available to form the sum j,
 
-bool subset_sum() {
-    for (int i = 0; i <= n;i++) {
-        dp[i][0] = true;
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (j - a[i - 1] >= 0) {
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - a[i - 1]];
+S[i-1][j] is the solution for the problem with the first i-1 numbers available to form the sum j,
+
+S[i-1][j-A[i]] is the solution for the problem with the first i-1 numbers available to form the sum j-A[i].
+*/
+
+int main()
+{
+    int A[] = {0, 1, 3, 5, 11}; // The first entry is a dummy
+
+    int n = 4;
+    int W = 6;
+
+    bool S[n+1][W+1];
+
+    for (int i=0; i<=n; i++) S[i][0] = true;
+    for (int j=1; j<=W; j++) S[0][j] = false;
+
+    for (int j=1; j<=W; j++) {
+        for (int i=1; i<=n; i++){
+            if (j < A[i]){  // we cannot use A[i]
+
+                S[i][j] = S[i-1][j];
             }
-            else{
-                dp[i][j] = dp[i - 1][j];
+            else {     // we can use A[i]
+
+                S[i][j] = S[i-1][j] || S[i-1][j-A[i]];
             }
         }
     }
-    return dp[n][sum];
-}
+    if (S[n][W]) {
+        cout << "We can represent W = " << W << " by a subset sum of A.\n";
+    }
+    else {
+        cout << "We cannot represent W = " << W << " by a subset sum of A.\n";
+    }
 
-int main() {
-      sum = 11;
-      bool dp[n + 1][sum + 1];
 
-      bool can = subset_sum();
-      printf("%s", can ? "Yes" : "No");
-      return 0;
+    cout << "Printing the subset:\n";
+    if (S[n][W]) {
+        int j = W;
+        for (int i=n; i>=1; i--) {
+            if (!S[i-1][j]){
+                cout << "Pick A[" << i << "] = " << A[i] << "\n";
+                j -= A[i];
+            }
+        }
+    }
+
+    return 0;
 }
 
